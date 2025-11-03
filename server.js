be757d6529358a6df7589ca58d0251d76a4b3a2a
ArjunlalDotwibe <arjund@twibe@3>
2025-11-03 20:29:50 +0530

    up

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://hanois.dotwibe.com"],
-    credentials: true,
+    origin: "http://localhost:3000",
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
 
-
-app.options(/.*/, cors());
 
 app.use('/api/users', userRoutes);
 
