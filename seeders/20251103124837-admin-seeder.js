5f58429115df65f73090108e3e08bca3c40caa0a
ArjunlalDotwibe <arjund@twibe@3>
2025-11-03 18:22:28 +0530

    up

--- seeders/20251103124837-admin-seeder.js
+++ seeders/20251103124837-admin-seeder.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
+const bcrypt = require("bcrypt");
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash("H@NNoi", 10);
+
+    await queryInterface.bulkInsert(
+      "Admins",
+      [
+        {
+          name: "Super Admin",
+          email: "admin@hanois.com",
+          password: hashedPassword,
+          created_at: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Admins", { email: "admin@hanois.com" }, {});
+  },
 };
