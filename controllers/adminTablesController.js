b2ecedca562832f27dc79a352ecbb6645bb611a0
ArjunlalDotwibe <arjund@twibe@3>
2025-11-04 13:32:16 +0530

    up

--- controllers/adminTablesController.js
+++ controllers/adminTablesController.js
@@ -32,4 +32,45 @@ const getTableSchema = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllTables, getTableSchema };
+const getAllTablesWithSchema = async (req, res, next) => {
+  try {
+
+    const tablesResult = await pool.query(`
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public'
+      ORDER BY table_name;
+    `);
+
+    const tables = tablesResult.rows;
+
+    const fullSchema = await Promise.all(
+      tables.map(async (t) => {
+        const columnsResult = await pool.query(
+          `
+          SELECT column_name, data_type, is_nullable, column_default
+          FROM information_schema.columns
+          WHERE table_name = $1
+          ORDER BY ordinal_position;
+          `,
+          [t.table_name]
+        );
+
+        return {
+          table: t.table_name,
+          columns: columnsResult.rows,
+        };
+      })
+    );
+
+    res.json({
+      success: true,
+      data: fullSchema,
+    });
+  } catch (err) {
+    console.error("Error fetching table schema:", err);
+    next(err);
+  }
+};
+
+module.exports = { getAllTables, getTableSchema ,getAllTablesWithSchema};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { loginAdmin, getAdmins,logoutAdmin } = require("../controllers/adminController");
 const { authenticateToken } = require('../middleware/auth');
 
-const { getAllTables } = require("../controllers/adminTablesController");
+const { getAllTables,getAllTablesWithSchema } = require("../controllers/adminTablesController");
 
 router.post("/login", loginAdmin);
 router.get('/', authenticateToken, getAdmins);
 
 router.post("/logout",logoutAdmin);
 
-router.get("/tables", getAllTables);
+router.get("/tables", getAllTablesWithSchema);
 
 
 module.exports = router;
