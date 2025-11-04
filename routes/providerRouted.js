8348902e5c557c5edc27935df8eb4e4b23aff76a
ArjunlalDotwibe <arjund@twibe@3>
2025-11-04 18:52:51 +0530

    up

--- routes/providerRouted.js
+++ routes/providerRouted.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerProvider } = require('../controllers/providerController');
+const { registerProvider,resetPassword } = require('../controllers/providerController');
 const { providerValidation } = require('../middleware/validation');
 
 router.post('/register', providerValidation, registerProvider);
+
+
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
