db0b81a384d58549f6255190c9058ad75e824f9e
supportdotwibe <support@dotwibe.com>
2025-11-06 15:07:56 +0530

    upp

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const CategoryController = require('../controllers/categoryController');
+// const express = require('express');
+// const router = express.Router();
+// const CategoryController = require('../controllers/categoryController');
 
-// Use the static methods directly
-router.post('/', CategoryController.create);
-router.get('/', CategoryController.getAll);
-router.get('/:id', CategoryController.getById);
-router.put('/:id', CategoryController.update);
-router.delete('/:id', CategoryController.delete);
+// // Use the static methods directly
+// router.post('/', CategoryController.create);
+// router.get('/', CategoryController.getAll);
+// router.get('/:id', CategoryController.getById);
+// router.put('/:id', CategoryController.update);
+// router.delete('/:id', CategoryController.delete);
 
-module.exports = router;
+// module.exports = router;
