33e4d516f4f7ec74d7bdd1a55583a493edc23837
supportdotwibe <support@dotwibe.com>
2025-11-06 16:11:41 +0530

    up

---
+++ migrations/1762425614898_create-service-table.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('services', {
+    id: 'id', // shorthand for serial primary key
+    name: { type: 'text', notNull: true, unique: true },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('services');
+};
+
