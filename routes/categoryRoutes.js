f9cddc00fcaee91e5bf94a4fca9e2953f7eafd46
supportdotwibe <support@dotwibe.com>
2025-11-06 14:24:14 +0530

    up

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,25 +1,19 @@
-const express = require('express');
-const router = express.Router();
 
-const {
+ const express = require('express');
+ const router = express.Router();
+ const {
   createCategory,
   getCategories,
   getCategoryById,
   updateCategory,
-  deleteCategory,
-} = require('../controllers/categoryController');
-
-const { authenticateToken } = require('../middleware/auth');
-const { categoryValidation } = require('../middleware/validation');
-
-router.post('/', authenticateToken, categoryValidation, createCategory);
-
-router.get('/', getCategories);
-
-router.get('/:id', getCategoryById);
-
-router.put('/:id', authenticateToken, categoryValidation, updateCategory);
-
-router.delete('/:id', authenticateToken, deleteCategory);
-
-module.exports = router;
+  deleteCategory, } = require('../controllers/categoryController');
+ 
+ router.post('/', createCategory);
+ 
+ router.get('/', getCategories);
+ router.get('/:id', getCategoryById);
+ router.put('/:id', updateCategory);
+ router.delete('/:id', deleteCategory);
+ 
+ module.exports = router;
+  
