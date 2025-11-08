4f89b2b24c050fd262e962e27c8f95262f8c1d69
supportdotwibe <support@dotwibe.com>
2025-11-08 12:44:42 +0530

    yup

--- routes/providerRouted.js
+++ routes/providerRouted.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-const { registerProvider,
-    resetPassword, 
-    getProviders,
-    deleteProvider,
-    updateProvider,
-    getProviderById,
-    updateProviderProfile
-} = require('../controllers/providerController');
-const { providerValidation } = require('../middleware/validation');
-const { authenticateToken } = require('../middleware/auth');
+// const { registerProvider,
+//     resetPassword, 
+//     getProviders,
+//     deleteProvider,
+//     updateProvider,
+//     getProviderById,
+//     updateProviderProfile
+// } = require('../controllers/providerController');
+// const { providerValidation } = require('../middleware/validation');
+// const { authenticateToken } = require('../middleware/auth');
 
-router.post('/register', providerValidation, registerProvider);
-
-
-router.post('/reset-password', resetPassword);
-
-router.get('/', getProviders);
-
-router.delete('/:id', authenticateToken, deleteProvider);
-
-// router.put('/:id', authenticateToken, updateProvider);
-
-// router.get('/:id', authenticateToken, getProviderById);
-
-router.put('/update-profile/:providerId', authenticateToken, updateProviderProfile);
+// router.post('/register', providerValidation, registerProvider);
 
 
+// router.post('/reset-password', resetPassword);
 
-module.exports = router;
+// router.get('/', getProviders);
+
+// router.delete('/:id', authenticateToken, deleteProvider);
+
+// // router.put('/:id', authenticateToken, updateProvider);
+
+// // router.get('/:id', authenticateToken, getProviderById);
+
+// router.put('/update-profile/:providerId', authenticateToken, updateProviderProfile);
+
+
+
+// module.exports = router;
  
