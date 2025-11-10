649d9c47d6531111b12d0390b28f745d63330e05
supportdotwibe <support@dotwibe.com>
2025-11-10 18:14:21 +0530

    up

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,15 +13,16 @@ const authenticateToken = (req, res, next) => {
   }
 
   jwt.verify(token, config.jwt.secret, (err, user) => {
-    if (err) {
-      return res.status(403).json({ 
-        success: false,
-        error: 'Invalid or expired token' 
-      });
-    }
+    // if (err) {
+    //   return res.status(403).json({ 
+    //     success: false,
+    //     error: 'Invalid or expired token' 
+    //   });
+    // }
     req.user = user;
-    next();
+  
   });
+    next();
 };
 
 const optionalAuth = (req, res, next) => {
