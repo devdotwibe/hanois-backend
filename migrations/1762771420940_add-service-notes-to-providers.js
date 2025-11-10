140ecdf7147077a7fe93e29285fb192b5a65963c
supportdotwibe <support@dotwibe.com>
2025-11-10 16:13:58 +0530

    up

---
+++ migrations/1762771420940_add-service-notes-to-providers.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumn("providers", {
+    service_notes: {
+      type: "text",
+      notNull: false,
+    },
+  });
+};
+
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ */
+export const down = (pgm) => {
+  pgm.dropColumn("providers", "service_notes");
+};
