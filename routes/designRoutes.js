797ae1ce11192c4acc3d91d9aea7fec91cd89a1f
ArjunlalDotwibe <arjund@twibe@3>
2025-11-12 11:18:48 +0530

    up

--- routes/designRoutes.js
+++ routes/designRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const designController = require('../controllers/designController');
 
-// Use the static methods directly
 router.post('/', designController.create);
 router.get('/', designController.getAll);
 router.get('/:id', designController.getById);
