40187499b21a8f142a41959d93e750d81a13b45f
ArjunlalDotwibe <arjund@twibe@3>
2025-11-12 19:46:00 +0530

    up

---
+++ migrations/1762956206924_add-provider-id-to-work.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+
+    pgm.addColumns('providers', {
+
+        provider_id: { type: 'integer[]', notNull: false  },
+        service_id: { type: 'integer[]',  notNull: false },
+     
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+      pgm.dropColumns('providers', [
+        'provider_id',
+        'service_id'
+     ]);
+};
