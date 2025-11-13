b97dab6d8ce7a98d6c001ac3e9253655360d956b
ArjunlalDotwibe <arjund@twibe@3>
2025-11-13 13:02:50 +0530

    up

--- models/workModel.js
+++ models/workModel.js
@@ -45,7 +45,7 @@ class WorkModel {
             total_cost,
             created_at
         )
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,'Review Awaiting',NOW())
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,'Awaiting Review',NOW())
         RETURNING 
             id,
             user_id,
