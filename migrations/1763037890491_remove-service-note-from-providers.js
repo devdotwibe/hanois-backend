bba97616dcb3b3815e11d1f8207370acbb7a5851
jerindotwibe <jerindotwibe@outlook.com>
2025-11-20 11:23:11 +0530

    qww

--- migrations/1763037890491_remove-service-note-from-providers.js
+++ migrations/1763037890491_remove-service-note-from-providers.js
@@ -1,24 +1,24 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-export const shorthands = undefined;
+const shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {
+exports,up = (pgm) => {
   // Drop the 'service_notes' column from the 'providers' table
   pgm.sql('ALTER TABLE providers DROP COLUMN service_notes;');
 };
 
 /**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param  {import('node-pg-migrate').MigrationBuilder} pgm
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {
+exports.down = (pgm) => {
   // Add the 'service_notes' column back to the 'providers' table
   pgm.sql('ALTER TABLE providers ADD COLUMN service_notes TEXT;');
 };
