29337749a787460947660c4b245092d1a131c54f
jerindotwibe <jerindotwibe@outlook.com>
2025-11-25 15:39:57 +0530

    www

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/categoryController');
 
-// Use the static methods directly
+// Create category
 router.post('/', CategoryController.create);
-router.get('/', CategoryController.getAll);
-router.get('/:id', CategoryController.getById);
-router.put('/:id', CategoryController.update);
-router.delete('/:id', CategoryController.delete);
 
+// Get categories assigned to providers
+router.get('/', CategoryController.getAll);
+
+// ✅ MUST be before "/:id"
 router.get('/all', CategoryController.getAllCategories);
 
+// Get category by ID
+router.get('/:id', CategoryController.getById);
+
+// Update category
+router.put('/:id', CategoryController.update);
+
+// Delete category
+router.delete('/:id', CategoryController.delete);
 
 module.exports = router;
